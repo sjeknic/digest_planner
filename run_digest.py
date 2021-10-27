4f49ec85505418418ac72e1903e1d821b051a472
sjeknic <sjeknic@stanford.edu>
2021-10-27 15:32:41 -0700

    added command line args

--- run_digest.py
+++ run_digest.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import itertools
+import argparse
 
 import numpy as np
 from tabulate import tabulate
@@ -11,6 +12,36 @@ from Bio import Seq as bioseq
 # TODO: Add ability to load any file
 # TODO: Update enzyme list
 # TODO: Display information about buffers
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--single', '-s',
+        action='store_true',
+        help='Include single enzyme digests'
+    )
+    parser.add_argument(
+        '--double', '-d',
+        action='store_true',
+        help='Include double enzyme digests'
+    )
+    parser.add_argument(
+        '--num', '-n',
+        default=10,
+        type=int,
+        help='Number of results to display'
+    )
+    parser.add_argument(
+        '--file', '-f',
+        default='plasmid.txt',
+        type=str,
+        help='Name of file that contains plasmid sequence'
+    )
+
+    return parser.parse_args()
+
 
 def score_bands(band_list):
     # Taking the old cost function w/o modification
@@ -47,8 +78,16 @@ def _round(nums):
     # TODO: Dirty way of returning list with descending band size
     return [int(round(n, -2)) for n in nums][::-1]
 
+
 if __name__ == '__main__':
-# Load all restriction enzymes
+    # Load all restriction enzymes
+    args = parse_args()
+
+    if not args.single and not args.double:
+        # If both are false, just do default behavior
+        args.single = True
+        args.double = True
+
     with open('enzymes.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         enzdic = dict(reader)
@@ -65,7 +104,7 @@ if __name__ == '__main__':
     enzymes = singles + doubles
 
     # Load the plasmid
-    with open('plasmid.txt', 'r') as f_open:
+    with open(args.file, 'r') as f_open:
         plasmid_sequence = f_open.read()
     plasmid_sequence = plasmid_sequence.strip()
     plasmid_sequence = plasmid_sequence.upper()
@@ -88,26 +127,23 @@ if __name__ == '__main__':
         digest[str(enz)] = (score_bands(bands), bands)
 
     digest = {k: v for k, v in sorted(digest.items(), key=lambda x: x[1])}
-    single = {k: v for k, v in digest.items() if '+' not in k}
-    double = {k: v for k, v in digest.items() if '+' in k}
+
     # Display results
-    display = 10
+    display = args.num
     headers = ['Enzyme', 'Pattern', 'Score']
-    single_table = [tuple([k, _round(v[1]), v[0]])
-                    for n, (k, v) in enumerate(single.items())
-                    if n < display]
-    double_table = [tuple([k, _round(v[1]), v[0]])
-                    for n, (k, v) in enumerate(double.items())
-                    if n < display]
     print(f'Plasmid length: {len(plasmid_sequence)}')
-    print('Single enzymes')
-    print(tabulate(single_table, headers))
-    print('\n Double enzymes')
-    print(tabulate(double_table, headers))
+    if args.single:
+        single = {k: v for k, v in digest.items() if '+' not in k}
+        single_table = [tuple([k, _round(v[1]), v[0]])
+                        for n, (k, v) in enumerate(single.items())
+                        if n < display]
+        print('\n Single enzymes')
+        print(tabulate(single_table, headers))
 
-
-
-
-
-
-
+    if args.double:
+        double = {k: v for k, v in digest.items() if '+' in k}
+        double_table = [tuple([k, _round(v[1]), v[0]])
+                        for n, (k, v) in enumerate(double.items())
+                        if n < display]
+        print('\n Double enzymes')
+        print(tabulate(double_table, headers))
